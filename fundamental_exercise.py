c15e7c462d5c9604ea1d2e4cbc4bdd75682b8a1a
Thinh Doan <41156869+tdoan123@users.noreply.github.com>
2024-07-30 15:18:17 -0700

    update learning note for tuples

--- fundamental_exercise.py
+++ fundamental_exercise.py
@@ -322,4 +322,157 @@ print(' '.join(new_sentence))
 list = [10, 20, 13, 4]
 list.sort()
 
-list
+# tuples 
+# tuples: immutable, contain anyting
+
+t = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+type(t)
+t[9]
+
+t[2:8:3]
+
+for one_time in t: 
+    print (one_time, end=',')
+    
+t = ('abc','def', 'ghi', 'jkl')
+for one_time in t: 
+    print (one_time)
+    
+book1 = ('title1', 10.99)
+book2 = ('title2', 15.50)
+
+book1[1]
+'ghi' in t
+
+'g' in t 
+
+# tuples are immutable 
+
+# constant is different with immutable   
+
+sum([10,20,30,40,50])
+# tuple is more much more memories efficient  
+
+t = t + t
+t
+
+t += t 
+t
+
+t = (10, 20 , 30 )
+type(t)
+
+t = 10, 20, 30 
+
+t = (10) 
+type(t)
+# int
+# the reason is because () is acting as cover, similar to the way that doing () for math 
+# to make the tuple 
+t = (10,) # comma means: Im a tuple 
+type(t)
+
+# unpacking 
+# three different types of sequences: string, list, tuple
+mylist = [10, 20, 30]
+x = mylist  
+x
+
+x, y, z = mylist 
+x
+y
+z
+
+t = (100, 200, 300)
+x, y, z = t
+x
+y
+z
+
+title, price = book1
+title    
+price
+
+x = 100
+y = 200 
+
+y, x = x, y 
+x
+y
+
+
+s = 'abc'
+for one_item in enumerate(s):
+    print(one_item)
+
+'''
+(0, 'a')
+(1, 'b')
+(2, 'c')
+'''
+
+s = 'abc'
+for index, one_letter in enumerate(s):
+    print(f'{index}: {one_letter}')
+'''
+0: a
+1: b
+2: c
+'''    
+
+# converting to-from tuples 
+s = 'abcd'
+list(s) # turn the element of string 's' into a list, list run a for loop of s, then put into a list 
+tuple(s)
+
+mylist= [10, 20, 30, 40]
+tuple(mylist)
+# tuple run a for loop in my list then put the elements into it own tuple
+
+list(t) # turn a tuple from list  
+
+t = ('abc', 'def', 'ghi')
+'***'.join(t)
+# join() is a string method, not a list method because we can actual pass any sequent of string to join and it will work 
+
+# 60: sorting tuples 
+
+t = (30, 20, 40, 10, 7, 15)
+t.sort()
+
+sorted(t, reverse=False)
+# this sorted is a build in function rather than a method 
+mylist = [(5,3), (5,2), (4,8), (4,6)]
+sorted(mylist)
+
+
+# 61 - exercise - People 
+'''
+ask the user, repeatly, to enter a person's name (first name + space + lastname)
+Thinh Doan 
+Create a list of tuples in which each tuple has 2 elements 
+- last name
+- first name 
+
+[('thinh', 'doan'), ('last2', 'first2'), ('last3', 'first3')]
+- print the people's name, last name first (as a phone book), sorted by last name  
+- when they enter an empty string instead of a name, stop asking and print the name
+- if they enter a string without any space, scold them a bit
+'''
+phone_book = []
+while True: 
+    user_name = input('your first and last name is (first name + space + last name)').strip()
+    if not user_name:
+        break
+    if ' ' not in user_name: # damn it, why didnt I think about this one before :) 
+        print('hey follow through instruction')
+#    if user_name[-1] != ' ': # what is the better way to check on this?  
+#        print('hey not correct')
+    else: 
+        user_name2 = tuple(user_name.split(' '))
+        phone_book.append(user_name2)
+        print (user_name)
+        print (user_name2)
+print(phone_book)    
+
+
